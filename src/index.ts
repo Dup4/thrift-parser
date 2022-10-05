b31483e6b7967569be15f38da128d0f70190c345
Dup4 <lyuzhi.pan@gmail.com>
2022-10-05 11:22:35 +0800

    test: add simple test case

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,56 @@
-export const one = 1;
-export const two = 2;
+import {
+  CharStreams,
+  CharStream,
+  CommonTokenStream,
+  ParserRuleContext,
+} from "antlr4ts";
+import { BailErrorStrategy } from "antlr4ts";
+import { ThriftParser, DocumentContext } from "./antlr_gen/ThriftParser";
+import { ThriftLexer } from "./antlr_gen/ThriftLexer";
+
+export type ParserResult = [
+  ThriftLexer,
+  CommonTokenStream,
+  ThriftParser,
+  DocumentContext,
+];
+
+export function parse(inputStream: CharStream): ParserResult {
+  const lexer = new ThriftLexer(inputStream);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new ThriftParser(tokens);
+  parser.errorHandler = new BailErrorStrategy();
+
+  const ctx = new ParserRuleContext();
+  parser.enterRule(ctx, 0, 0);
+  const document = parser.document();
+  return [lexer, tokens, parser, document];
+}
+
+export class ThriftData {
+  lexer: ThriftLexer;
+  tokens: CommonTokenStream;
+  parser: ThriftParser;
+  document: DocumentContext;
+
+  constructor(inputStream: CharStream) {
+    const [lexer, tokens, parser, document] = parse(inputStream);
+
+    this.lexer = lexer;
+    this.tokens = tokens;
+    this.parser = parser;
+    this.document = document;
+  }
+
+  static fromString(data: string): ThriftData {
+    const inputStream = CharStreams.fromString(data);
+    return new ThriftData(inputStream);
+  }
+}
 
 export * from "./antlr_gen/ThriftLexer";
 export * from "./antlr_gen/ThriftListener";
 export * from "./antlr_gen/ThriftParser";
 export * from "./antlr_gen/ThriftVisitor";
+export * from "antlr4ts";
+export * from "antlr4ts/tree";
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,67 @@
 import { describe, expect, it } from "vitest";
-import { one, two } from "@/index";
 
-describe("should", () => {
-  it("exported", () => {
-    expect(one).toEqual(1);
-    expect(two).toEqual(2);
+import * as fs from "fs";
+import path from "path";
+
+import { ThriftData, TerminalNode } from "@/index";
+
+function getFullPath(file_name: string): string {
+  return path.resolve(__dirname, "test_data", file_name).toString();
+}
+
+describe("Thrift Parser", () => {
+  it("load files", () => {
+    const files = fs.readdirSync(getFullPath(""));
+
+    for (const file of files) {
+      const content = fs.readFileSync(getFullPath(file)).toString();
+      expect(content).matchSnapshot();
+    }
+  });
+
+  it("load thrift from string", function () {
+    const data = ThriftData.fromString('include "shared.thrift"');
+    expect(data.tokens.get(0).text).toStrictEqual("include");
+    const header = data.document.getChild(0);
+    const include = header.getChild(0);
+    const token = include.getChild(0);
+
+    expect(token.text).toStrictEqual("include");
+    expect(token instanceof TerminalNode).ok;
+    expect(token.childCount).toStrictEqual(0);
+  });
+
+  it("load literal value", function () {
+    const thrift = `const string default_user = 'it\\'s name is \\" x \\" or \\'x\\' \\r\\t\\n';`;
+    const data = ThriftData.fromString(thrift);
+    expect(data.tokens.get(0).text).toStrictEqual("const");
+
+    const defines = data.document.getChild(0);
+    const constValue = defines.getChild(0);
+    const token = constValue.getChild(0);
+
+    expect(token.text).toStrictEqual("const");
+    expect(token instanceof TerminalNode).ok;
+    expect(token.childCount).toStrictEqual(0);
+    expect(data.tokens.get(8).text).toStrictEqual(
+      `'it\\'s name is \\" x \\" or \\'x\\' \\r\\t\\n'`,
+    );
+  });
+
+  it("load literal value2", function () {
+    const thrift = `const string default_user = "\\'default_user\\'" ;
+      const string default_name = '"abc\\'s"';`;
+
+    const data = ThriftData.fromString(thrift);
+    expect(data instanceof ThriftData).ok;
+    expect(data.tokens.get(0).text).toMatchInlineSnapshot('"const"');
+  });
+
+  it("test complex literal more", () => {
+    const thrift = `const string default_user = "\\'default_user\\'" ;
+      const string default_name = '"abc\\'s"';`;
+    const data = ThriftData.fromString(thrift);
+    expect(data instanceof ThriftData).ok;
+    expect(data.tokens.get(0).text).toMatchInlineSnapshot('"const"');
   });
 });
